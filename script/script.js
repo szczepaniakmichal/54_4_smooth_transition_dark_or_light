810aecd72be00534bd6103e4c83248832d58a8bf
szczepaniakmichal <mssk87@gmail.com>
2019-12-02 21:27:59 +0100

    get event v2 works

--- script/script.js
+++ script/script.js
@@ -1,8 +1,28 @@
+// declare colors
 let r = 0;
+let g = 0;
+let b = 0;
 
+// get event and change color
 document.body.addEventListener('keydown', function (e) {
-    if (e.code === 'ArrowDown') {
-        r++;
-        console.log(r);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    if (e.code === 'ArrowUp') {
+        r += 5;
+        g += 5;
+        b += 5;
+        console.log(color);
+        if (r === g === b >= 255) {
+            r = g = b = 255;
+        }
+    } else if (e.code === 'ArrowDown') {
+        r -= 5;
+        g -= 5;
+        b -= 5;
+        console.log(color);
+        if ( r === g === b <= 0){
+            r = g = b = 0;
+        }
+
     }
+    document.body.style.backgroundColor = color;
 });
